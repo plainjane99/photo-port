cfb8bd6fa1f8c2a3c7063c43bbf29d6a5597a6bf
Plain Jane <jane99hsu@gmail.com>
2020-12-22 17:14:43 -0800

    added Gallery component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
 // this file is the center of the application, the root component that houses all other components
 
 // added this without instruction to match 20.1.5
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import About from './components/About';
 import Nav from './components/Nav';
+import Gallery from './components/Gallery';
 
 // this function returns html represented by JSX, Javascript XML
 // think of this like document.createElement(JSX)
+
+// instead of hardcoding all nav categories, we'll define the categories in an array
+// of objects that contain each category's name and a short description. 
+// use the useState hook here so we can have the option to change the categories at some point in the future
+
 function App() {
+  const [categories] = useState([
+    {
+      name: "commercial",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects"
+    },
+    {
+      name: "portraits",
+      description: "Portraits of people in my life"
+    },
+    {
+      name: "food",
+      description: "Delicious delicacies"
+    },
+    {
+      name: "landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <div>
-      <Nav></Nav>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Nav>
       <main>
-        <About></About>
+        <div>
+          <Gallery></Gallery>
+          <About></About>
+        </div>
       </main>
     </div>
   );
 }
-
 export default App;
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,35 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
 // React components must always return a single parent JSX element.
 // However, this element may have many children elements
 // For example, we cannot have another element outside and at the same level as <section>
-function Nav() {
-    // instead of hardcoding all nav categories, we'll define the categories in an array
-    // of objects that contain each category's name and a short description. 
-    const categories = [
-        {
-            name: "commercial",
-            description:
-                "Photos of grocery stores, food trucks, and other commercial projects"
-        },
-        {
-            name: "portraits",
-            description: "Portraits of people in my life"
-        },
-        {
-            name: "food",
-            description: "Delicious delicacies"
-        },
-        {
-            name: "landscape",
-            description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-        },
-    ];
+function Nav(props) {
 
-    // event listener function
-    function categorySelected(name) {
-        console.log(`${name} clicked`);
-    }
+    const {
+        categories = [],
+        setCurrentCategory,
+        currentCategory,
+    } = props;
+
+    // the first argument is the callback function, 
+    // the second argument is an array with a single element, currentCategory. 
+    // The second argument directs the hook to re-render the component on changes to the value of this state
+    useEffect(() => {
+        document.title = capitalizeFirstLetter(currentCategory.name);
+    }, [currentCategory]);
 
     // map() over the 'categories' array
     // in JSX, we need to give each element in the array a unique key attribute
@@ -37,7 +25,7 @@ function Nav() {
     // the parantheses returns back a single JSX element 
     // we also add an event listener since we'll want to use the links we put into the nav
     return (
-        <header>
+        <header className="flex-row px-1">
             <h2>
                 <a data-testid="link" href="/">
                     <span role="img" aria-label="camera"> 📸</span> Oh Snap!
@@ -55,12 +43,18 @@ function Nav() {
                     </li>
 
                     {categories.map((category) => (
+                        // short-circuit expression
+                        // currentCategory.name === category.name will get evaluated, 
+                        // and as long as it is true, then the second bit of the short circuit, navActive, will be returned
                         <li
-                            className="mx-1"
-                            key={category.name}
-                        >
-                            <span onClick={() => categorySelected(category.name)}>
-                                {category.name}
+                            className={`mx-1 ${currentCategory.name === category.name && 'navActive'}`}
+                            key={category.name}>
+                            <span
+                                onClick={() => {
+                                    setCurrentCategory(category)
+                                }}
+                            >
+                                {capitalizeFirstLetter(category.name)}
                             </span>
                         </li>
                     ))}
